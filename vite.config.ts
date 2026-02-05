dbc08eb3b290f13bff5a8758ac0189a7ce0d7825
GratitechHD <allenyan77@gmail.com>
2026-02-05 12:51:09 -0500

    chore: update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/vine-lingo/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
